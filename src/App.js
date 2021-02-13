64d64cf11b8ea34650729decde35e62cbf8fa223
Ashutosh Harkare <ashutoshharkare74.com>
2021-02-13 10:40:52 +0530

    Section 1 - (Component basics, props and state) #1

--- src/App.js
+++ src/App.js
@@ -18,12 +18,22 @@ class App extends Component {
   }
 
   render() {
+    const styleButton = {
+      backgroundColor : 'white',
+      padding : '8px',
+      border : '1px solid blue',
+      font : 'inherit',
+      cursor : 'pointer',
+    };
     return (
       <div className="App">
         <h1> My first React app </h1>
-        <Person name="Ashutosh Harkare" age="21" click= {() => this.switchButtonHandler("Ashutosh!")} change = {this.onChangeHandler}> My hobbies : Cricket</Person>
-        <button onClick = {this.switchButtonHandler.bind(this, "Prashant Agrawal")}>Switch</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} click= {() => this.switchButtonHandler("Ashutosh!")} change = {this.onChangeHandler}> My hobbies : Cricket</Person>
+        <button style={styleButton} onClick = {this.switchButtonHandler.bind(this, "Prashant Agrawal")}>Switch</button>
         <p> {this.state.person[0].name}</p>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} click= {() => this.switchButtonHandler("Ashutosh!")} change = {this.onChangeHandler}> My hobbies : Cricket</Person>
+        <button style={styleButton} onClick = {this.switchButtonHandler.bind(this, "Prashant Agrawal")}>Switch</button>
+        <p> {this.state.person[1].name}</p>
       </div>
     );
   }
